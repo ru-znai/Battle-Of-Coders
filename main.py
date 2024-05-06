74b2dce5fc7093aaa37116e170263ac9cf896627
Acer-IT <rk3wwc@gmail.com>
2024-05-06 10:30:51 +0300

    работа с /login кнопка help popup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import sqlite3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session, redirect, url_for, request
 import files
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key_here'
 test_cases_even_or_odd = [[[1, 2], 2], [[2, 4], 8], [[-1, 6], -6]]
 
 
@@ -55,13 +56,25 @@ def index():
 
     return render_template('description.html', data=data, current_user_lvl=current_user_lvl)
 
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Обработка данных из формы
+        username = request.form['username']
+        password = request.form['password']
+        # Дополнительная обработка или проверка данных
+
+        # Вернуть какой-то результат или перенаправление
+        return 'Данные получены: {} {}'.format(username, password)
+
+    # Если метод GET, просто отобразить страницу
     return render_template('login.html')
 
 @app.route('/intro', methods=['POST', 'GET'])
 def intro():
     return render_template('intro.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/scripts.js
+++ static/scripts.js
@@ -29,3 +29,5 @@ document.addEventListener('DOMContentLoaded', () => {
     // Запускаем функцию показа всплывающих надписей при загрузке страницы
     showHints();
 });
+
+
